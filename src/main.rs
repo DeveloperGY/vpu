f479629b533ccf2efe70f4725dea4ca93a05213b
DeveloperGY <dev.garrettyenojr@gmail.com>
2023-02-01 14:06:50 -0500

    Began file reading

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,34 @@ mod cpu;
 
 use cpu::CPU;
 
-// use std::env::Args; For when its time to load the binary
+use std::env;
+use std::fs;
+use std::io::prelude::*;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Error: Please pass a binary file!");
+        return;
+    }
+    
+    let mut file_ptr = match fs::File::open(&args[1]) {
+        Ok(f) => f,
+        Err(_) => {
+            eprintln!("Error: Binary not found!");
+            return;
+        }  
+    };
+    
+    let mut file: String = String::new();
+    match file_ptr.read_to_string(&mut file) {
+        Ok(_) => (),
+        Err(_) => {
+            eprintln!("Error: Not valid fpu binary!");
+            return;
+        }
+    };
+    
     let mut cpu: CPU = CPU::new(255, 255);
     cpu.run();
 }
