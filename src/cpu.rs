3ddc7a1a337b444832d37a6cb7ef95c27cbb7f49
DeveloperGY <dev.garrettyenojr@gmail.com>
2023-02-01 12:43:48 -0500

    Added execute cycle

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,37 +2,121 @@ pub mod mem;
 
 use mem::MEM;
 
+struct Instruction {
+    opcode: u8,
+    dest: u8,
+    src: u8,
+    src1: u8,
+    src2: u8,
+    address: u8,
+    immediate: u8
+}
+
 pub struct CPU {
     regs: [i8; 4],
     ram: MEM<i8>,
-    rom: MEM<i16>,
+    rom: MEM<u16>,
     
-    ip: u8
+    ins_ptr: u8,
+    equal: bool,
+    running: bool
 }
 
 impl CPU {
     pub fn new(rom_capacity: u8, ram_capacity: u8) -> CPU {
         let regs: [i8; 4] = [0; 4];
-        let ram: MEM<i8> = MEM::new(ram_capacity);
-        let rom: MEM<i16> = MEM::new(rom_capacity);
-        let ip: u8 = 0b00000000;
+        let ram: MEM<i8> = MEM::new(ram_capacity, 0);
+        let rom: MEM<u16> = MEM::new(rom_capacity, 0);
+        let ins_ptr: u8 = 0b00000000;
+        let running: bool = true;
+        let equal:bool = true;
         
-        CPU {regs, ram, rom, ip}
+        
+        CPU {regs, ram, rom, ins_ptr, equal, running}
     }
     
     pub fn run(self: &mut Self) {
-        let instruction: i16 = self.fetch();
-        self.decode(instruction);
+        while self.running
+        {
+            let instruction: Result<u16, ()> = self.fetch();
+            let instruction: u16 = match instruction {
+                Ok(i) => i,
+                Err(()) => {
+                    self.running = false;
+                    break;
+                }  
+            };
+            let instruction: Instruction = self.decode(instruction);
+            self.execute(instruction);   
+        }
     }
     
-    fn fetch(self: &mut Self) -> i16
-    {
-        let instruction: i16 = self.rom.arr[self.ip as usize];
-        self.ip += 1;
-        instruction
+    fn fetch(self: &mut Self) -> Result<u16, ()> {
+        let instruction: u16 = self.rom.arr[self.ins_ptr as usize];
+        if self.ins_ptr as usize + 1 > u8::MAX as usize {
+            return Err(());
+        }
+        self.ins_ptr += 1;
+        Ok(instruction)
     }
     
-    fn decode(self: &Self, instruction: i16) {
-        let _val: i16 = instruction;
+    fn decode(self: &Self, instruction: u16) -> Instruction {
+        let opcode: u8 = ((instruction & 0b1111000000000000) >> 12) as u8;
+        let dest: u8 = ((instruction & 0b0000110000000000) >> 10) as u8;
+        let src: u8 = ((instruction & 0b0000001100000000) >> 8) as u8;
+        let src1: u8 = ((instruction & 0b0000000011000000) >> 6) as u8;
+        let src2: u8 = ((instruction & 0b0000000000110000) >> 4) as u8;
+        let address: u8 = (instruction & 0b0000000011111111) as u8;
+        let immediate: u8 = (instruction & 0b0000000011111111) as u8;
+        Instruction{opcode, dest, src, src1, src2, address, immediate}
+    }
+    
+    fn execute(self: &mut Self, instruction: Instruction) {
+        match instruction.opcode {
+            0b0000 => (),
+            0b0001 => {
+                // HLT
+                self.running = false;
+            },
+            0b0010 => {
+                // ADD
+                self.regs[instruction.dest as usize] = self.regs[instruction.src1 as usize].wrapping_add(self.regs[instruction.src2 as usize]);
+            },
+            0b0011 => {
+                // SUB
+                self.regs[instruction.dest as usize] = self.regs[instruction.src1 as usize].wrapping_sub(self.regs[instruction.src2 as usize]);
+            },
+            0b0100 => {
+                // AND 
+                self.regs[instruction.dest as usize] = self.regs[instruction.src1 as usize] & self.regs[instruction.src2 as usize]; 
+            },
+            0b0101 => {
+                // OR
+                self.regs[instruction.dest as usize] = self.regs[instruction.src1 as usize] | self.regs[instruction.src2 as usize]; 
+            },
+            0b0110 => {
+                // XOR
+                self.regs[instruction.dest as usize] = self.regs[instruction.src1 as usize] ^ self.regs[instruction.src2 as usize]; 
+            },
+            0b0111 => {
+                // STR
+                self.ram.arr[instruction.address as usize] = self.regs[instruction.src as usize];
+            },
+            0b1000 => {
+                // LOD
+                self.regs[instruction.dest as usize] = self.ram.arr[instruction.address as usize];
+            },
+            0b1001 => {
+                // JPE
+                if self.equal {
+                    self.ins_ptr = instruction.address;
+                }
+            },
+            0b1010 => {
+                // LDI
+                self.regs[instruction.dest as usize] = instruction.immediate as i8;
+            },
+            _ => ()
+        }
     }
 }
--- src/cpu/mem.rs
+++ src/cpu/mem.rs
@@ -2,9 +2,13 @@ pub struct MEM<T> {
     pub arr: Box<[T]>
 }
 
-impl<T> MEM<T> {
-    pub fn new(capacity: u8) -> MEM<T> {
-        let mem_vec: Vec<T> = Vec::with_capacity(capacity as usize);
+impl<T: Copy + Clone> MEM<T> {
+    pub fn new(capacity: u8, default_value: T) -> MEM<T> {
+        let mut mem_vec: Vec<T> = Vec::with_capacity(capacity as usize);
+        for _x in 0..=capacity {
+            mem_vec.push(default_value);
+        }
+        
         let mem_arr: Box<[T]> = mem_vec.into_boxed_slice();
         MEM {arr: mem_arr}        
     }
